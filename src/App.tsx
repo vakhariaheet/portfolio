df992d360179d09c2c5b969434debc4f304c7317
Heet Vakharia <heetkv@gmail.com>
2022-06-27 18:24:30 +0530

    Fixed issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import React, { useEffect } from 'react';
 import './App.scss';
 
 function App() {
-	useEffect(() => {
-		setTimeout(() => {
-			window.location.replace('https://linktr.ee/vakharia_heet');
-		}, 5000);
-	}, []);
+	
 	return (
 		<div className='container'>
 			<p>Portfolio</p>
 			<p className='comming-soon'>Comming Soon</p>
-			<p>Page will be automatically redirect to Lintr.ee in 5s</p>
+
 			<a href='https://linktr.ee/vakharia_heet'>My Projects(Linktree)</a>
 		</div>
 	);
