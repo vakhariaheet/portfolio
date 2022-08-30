f67da3d68aaf009396d6d39a6f3c8983c7eb3299
Heet Vakharia <heetkv@gmail.com>
2022-08-30 13:11:37 +0530

    Added Clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -57,6 +57,11 @@ const Document = () => {
 					gtag('config', 'G-Z33V64VG00');
 				`}
 				</Script>
+				<Script id={'clarity'}>{`function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "dfo4awrivk");`}</Script>
 			</Head>
 			<body>
 				<Main />
