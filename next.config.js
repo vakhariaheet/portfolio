4ebc76aeb4d4b3600ef4ab3a703918e7990e7930
Heet Vakharia <heetkv@gmail.com>
2022-11-25 14:58:29 +0530

    Added SiteMap and fixed Clarity

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	disable: process.env.NODE_ENV === 'development',
+});
 
 module.exports = withPWA({
-	pwa: {
-		dest: 'public',
-		disable: process.env.NODE_ENV === 'development',
-	},
 	reactStrictMode: true,
 	swcMinify: true,
 });
